1c4e99f122b664ea2d26ca3b7edd5a2b17aec02c
Bryan Sills <bryan.a.sills@gmail.com>
2015-06-18 20:53:38 -0500

    initial commit.

---
+++ app/src/main/java/ninja/bryansills/retroscrobble/MainActivity.java
@@ -0,0 +1,67 @@
+package ninja.bryansills.retroscrobble;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button authenticateButton = (Button) findViewById(R.id.button_authenticate);
+        final Button nowPlayingButton = (Button) findViewById(R.id.button_now_playing);
+        final Button scrobbleButton = (Button) findViewById(R.id.button_scrobble);
+
+        authenticateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nowPlayingButton.setEnabled(true);
+                scrobbleButton.setEnabled(true);
+            }
+        });
+
+        nowPlayingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Now playing", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        scrobbleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Scrobble", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
